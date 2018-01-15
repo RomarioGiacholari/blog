8d47e478ffe84fc3e02c829c324babd59398898e
RomarioGiacholari <giacholari@gmail.com>
2018-01-15 14:20:31 +0000

    forelse instead foreach on posts.index and migrated database

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,13 +2,15 @@
 @section('content')
 <div class="container">
          <div id="pinBoot">
-           @foreach($posts as $post)
+           @forelse($posts as $post)
                 <div class="thumbnail white-panel">
                 <a href="{{ route('posts.show',['post' => $post]) }}">{{ $post->title }} </a>
                     <hr>
                      <p class="post-body">{{ $post->body }}</p> 
                 </div>
-            @endforeach
+            @empty
+            <p>Blog posts coming soon.</p>
+            @endforelse
         </div>
     <div style="padding:80px">
         {{ $posts->links() }}
