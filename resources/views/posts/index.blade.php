f608e24e84d78374c474e54766cc52f544c4e6b9
RomarioGiacholari <giacholari@gmail.com>
2018-04-09 23:12:25 +0100

    style changes in posts.index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+
+    <h1 style="font-family:Comic Sans MS"><u>Posts</u></h1>
+    <hr />
+
     <div id="pinBoot">
         @forelse($posts as $post)
             <div class="thumbnail white-panel">
