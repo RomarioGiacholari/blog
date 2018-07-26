7ea6c75aace9f1cf73d30acee264804f5b08bbf3
RomarioGiacholari <giacholari@gmail.com>
2018-07-26 21:20:33 +0100

    added more skills and tools and changes intro paragrpah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,9 @@
                             <div>
                                 <h1>Hi! I am Mario.</h1>
                                 <p>
-                                I am an ambitious student and an aspiring web developer. I've just completed my internship at <a href="https://beautifulcanoe.com/">Beautiful Canoe</a> as a web developer on a project using <a href="https://laravel.com/">Laravel</a>.
-                                At the moment I am experimenting with React.js and Vue.js. I am also actively looking for a placement in the tech industry, starting on Summer 2018.
+                                 I am an ambitious student and an aspiring web developer. I completed my internship at <a href="https://beautifulcanoe.com/">Beautiful Canoe</a> as a web developer on a project using <a href="https://laravel.com/">Laravel</a> last year.
+                                 At the moment I am experimenting with React.js, Vue.js and I do work as a full stack web developer at <a href="https://www.bootcampmedia.co.uk/">Bootcampmedia</a>.
+                                 I am also happy to announce that I secured a placement with <a href="https://www.pinewood.co.uk/">Pinewood Technologies</a> for the year to come.
                                 </p>
                                 <!-- <h3><span class="label label-default">Skills & Projects</span></h3> -->
                                 <p>Those are some of the skills and tools I use to build and monitor web apps.</p>
@@ -18,12 +19,14 @@
                                 <span class="label label-info">AWS</span> <span class="label label-info">Heroku</span>
                                 <span class="label label-info">Java</span> <span class="label label-info">Testing</span>
                                 <span class="label label-info">Git</span> <span class="label label-info">SQL</span>
-                                <span class="label label-info">Ajax</span> <span class="label label-info">JQuery</span>
-                                <span class="label label-info">React - basic</span> <span class="label label-info">Vue</span>
-                                <span class="label label-info">Bash</span> <span class="label label-info">MVC</span>  <span class="label label-info">Google Analytics</span>
+                                <span class="label label-info">Ajax</span>  <span class="label label-info">React - basic</span>
+                                 <span class="label label-info">Vue</span> <span class="label label-info">Bash</span>
+                                 <span class="label label-info">MVC</span>  <span class="label label-info">Google Analytics</span>
+                                <span class="label label-info">Python</span> <span class="label label-info">Digital Ocean</span>
+                                <span class="label label-info">Nginx</span>
                                 </p>
                             </div>
-                            <p><b><u>Here are some of the projects I have contributed to.</u></b></p>
+                            <h3>Here are some of the projects I have contributed to ...</h3>
                             <section>
                                 <project-list></project-list>
                            </section>
