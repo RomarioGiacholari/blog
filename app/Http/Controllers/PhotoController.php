7ab1d6c207e581b94fceec54231bf0ba1c052c35
giacholari <giacholari@gmail.com>
2019-11-16 00:06:41 +0000

    add null check on photos

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use stdClass;
 use Storage;
 
 class PhotoController extends Controller
 {
     public function index()
     {
+        $viewModel = new stdClass;
+        $viewModel->photos = null;
         $files = Storage::disk('public')->files();
-        
-        $photos = array_filter($files, function ($file) {
-            return strpos($file, 'jpg');
-        });
-        
-        return view('photos.index', ['photos' => $photos]);
+
+        if ($files !== null && count($files) > 0) 
+        {
+            $photos = array_filter($files, function ($file) {
+                return strpos($file, 'jpg');
+            });
+            
+            if ($photos !== null && count($photos) > 0)
+            {
+                $viewModel->photos = $photos;
+            }
+        }
+
+        return view('photos.index', ['viewModel' => $viewModel]);
     }
 }
