87c0108c3443f96e9b5e073ce70092c6d3e6f581
RomarioGiacholari <giacholari@gmail.com>
2019-12-09 20:39:03 +0000

    added eye-contractor - notebook && sparehouse-live projects

--- resources/assets/js/components/Project-list.vue
+++ resources/assets/js/components/Project-list.vue
@@ -19,7 +19,10 @@
                     {id:2, name:"Astonroom", link:"http://astonroom.herokuapp.com/", image:"photos/astonroom.png"},
                     {id:3, name:"Industry Club", link:"https://cs.aston.ac.uk/industryclub/", image:"photos/cs-industry-portal.png"},
                     {id:4, name:"Picarea", link:"https://picarea.github.io/Porftolio/", image:"photos/picarea.png"},
-                    {id:5, name:"Astonbrite", link:"https://github.com/RomarioGiacholari/eventbrite", image:"photos/astonbrite.png"},
+                    {id:5, name:"Eye-Contractor", link:"https://www.eye-contractor.co.uk", image:"photos/eye-contractor.png"},
+                    {id:6, name:"Sparehouse-live", link:"https://www.sparehouse.live", image:"photos/sparehouse-live.png"},
+                    {id:7, name:"Astonbrite", link:"https://github.com/RomarioGiacholari/eventbrite", image:"photos/astonbrite.png"},
+                    {id:8, name:"Personal CRM", link:"https://github.com/RomarioGiacholari/notebook", image:"photos/notebook.png"},
                 ]
             }
         }
