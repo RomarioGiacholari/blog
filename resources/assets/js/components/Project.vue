865558d51843142df8db0ef1ba9ed4e64cd0efe2
RomarioGiacholari <giacholari@gmail.com>
2019-12-18 13:43:27 +0000

    alter link for project - astonroom to point to github repo and run - npm run prod

--- resources/assets/js/components/Project.vue
+++ resources/assets/js/components/Project.vue
@@ -1,17 +1,16 @@
 <template>
-        <div class="thumbnail projects white-panel">
-            <a :href="link">
-                <img :src="image" :alt="name">
-            </a>
-           <div class="caption">
-               <p>{{name}}</p>
-          </div>
-       </div>
+    <div class="thumbnail projects white-panel">
+        <a :href="link">
+            <img :src="image" :alt="name">
+        </a>
+        <div class="caption">
+            <p>{{ name }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-
         props:['link','image','name'],
     }
 </script>
