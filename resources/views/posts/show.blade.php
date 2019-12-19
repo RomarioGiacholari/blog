962b43ac64f39d8f553d6cc9f3a529a9dbb2ee9b
RomarioGiacholari <giacholari@gmail.com>
2019-12-19 16:07:22 +0000

    include creator of post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            @if($viewModel !== null && $viewModel->post !== null)
+            @if($viewModel !== null && $viewModel->post !== null && $viewModel->author !== null)
             <h1 style="font-family:Comic Sans MS;"> <u> {{ $viewModel->post->title }} </u> </h1>
 
-            {{ $viewModel->post->created_at->diffForHumans() }}
+            {{ $viewModel->post->created_at->diffForHumans() }} by {{ $viewModel->author }}
 
             <hr>
             <p class="post-show-body"> {{ $viewModel->post->body }} </p>
