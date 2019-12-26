fb2f5802e34adf90c90a5a4c3ece624142e97321
RomarioGiacholari <giacholari@gmail.com>
2019-12-26 14:14:05 +0000

    add title attribute on photos and set friendlyPhotoName

--- resources/views/photos/show.blade.php
+++ resources/views/photos/show.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
-@if($viewModel != null && $viewModel->pageTitle !== null && $viewModel->photo !== null)
+@if($viewModel != null && $viewModel->pageTitle !== null && $viewModel->photo !== null && $viewModel->photoFriendlyName !== null)
 @section('title', $viewModel->pageTitle)
 @section('content')
 <div class="container">
-    <h1 style="font-family:Comic Sans MS"><u>Photo identifier:</u></h1>
-    <p>{{ $viewModel->photo }}</p>
+    <h1 style="font-family:Comic Sans MS"><u>{{ $viewModel->photoFriendlyName }}</u></h1>
     <hr />
     <div class="row">
         <div class="col-md-12">
-            <img src="{{ asset($viewModel->photo) }}" class="img-responsive" alt="{{ $viewModel->photo }}">
+            <img src="{{ asset($viewModel->photo) }}" class="img-responsive" title="{{ $viewModel->photoFriendlyName }}" alt="{{ $viewModel->photoFriendlyName }}">
         </div>
     </div>
 </div>
