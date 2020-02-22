21e0f7dbdbe5b5b4647d51c055733d93d423c3d1
RomarioGiacholari <giacholari@gmail.com>
2020-02-22 16:09:43 +0000

    remove spinner and extract google analytics js script

--- public/js/spinner.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function () {
-    setTimeout(function () {
-        var spinner = document.getElementsByClassName("fa-spin")[0];
-        var body = document.querySelector("body");
-        body.classList.remove("opacity-is-loading");
-        spinner.classList.add("hidden");
-    }, 200);
-});
