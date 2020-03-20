b6256a77c5efa236b4fffab35cd864bdb0b24783
RomarioGiacholari <giacholari@gmail.com>
2020-03-20 17:15:57 +0000

    add section with id to target content

--- resources/views/privacy-policy/index.blade.php
+++ resources/views/privacy-policy/index.blade.php
@@ -7,20 +7,22 @@
         <div class="col-md-12">
             <h1>Privacy policy</h1>
             <hr />
-            <p class="font-size-16"> {{ $viewModel->introduction }} </p>
+            <section id="introduction">
+                <p class="font-size-16"> {{ $viewModel->introduction }} </p>
+            </section>
 
             @foreach($viewModel->content as $title => $text)
+                <section id="{{ strtolower($title) }}">
+                    <h3>{{ $title }}</h3>
 
-            <h3>{{ $title }}</h3>
-
-            @if($title == 'Contact')
-            <p class="font-size-16">
-                {{ $text }} <a href="mailto:{{ $viewModel->contactEmail }}">{{ $viewModel->contactEmail }}</a>.
-            </p>
-            @else
-            <p class="font-size-16">{{ $text }}</p>
-            @endif
-
+                    @if($title == 'Contact')
+                    <p class="font-size-16">
+                        {{ $text }} <a href="mailto:{{ $viewModel->contactEmail }}">{{ $viewModel->contactEmail }}</a>.
+                    </p>
+                    @else
+                    <p class="font-size-16">{{ $text }}</p>
+                    @endif
+                </section>
             @endforeach
         </div>
     </div>
