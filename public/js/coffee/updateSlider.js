9ba0e6c8f42ac9be31aeb4d71d8a7c6039b62e13
RomarioGiacholari <giacholari@gmail.com>
2020-04-07 14:05:31 +0100

    remove slider and bring back input of type number

--- public/js/coffee/updateSlider.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var slider = document.getElementById("amount");
-    var output = document.getElementById("currentAmount");
-    output.innerHTML = slider.value;
-
-    slider.oninput = function () {
-        output.innerHTML = this.value;
-    }
-});
--- resources/views/coffee/index.blade.php
+++ resources/views/coffee/index.blade.php
@@ -17,7 +17,7 @@
 
                 <div class="form-group">
                     <label for="amount">Amount in (£) <span id="currentAmount"></span></label>
-                    <input type="range" name="amount" id="amount" value="{{ old('amount') ?? 1 }}" min="1" max="100" step="1" required>
+                    <input class="form-control" type="number" name="amount" id="amount" value="{{ old('amount') }}" min="1" required>
                 </div>
                 
                 <div class='form-group'>
@@ -31,5 +31,5 @@
 </div>
 @endsection
 @section('scripts')
-<script src="{{ asset('js/coffee/updateSlider.js') }}" defer></script>
+<script src="{{ asset('js/coffee/updateAmount.js') }}" defer></script>
 @endsection
