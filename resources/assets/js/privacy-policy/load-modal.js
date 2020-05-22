ea641211d9cea8e0f9a3e85564862a1b12777e09
RomarioGiacholari <giacholari@gmail.com>
2020-05-22 20:18:07 +0100

    do not load privacy policy modal when in privacy policy page

--- resources/assets/js/privacy-policy/load-modal.js
+++ resources/assets/js/privacy-policy/load-modal.js
@@ -3,37 +3,41 @@ var utilities = require('./../utilities/cookies').default;
 document.addEventListener("DOMContentLoaded", function () {
     var cookieName = '__privacy';
     var isCookie = utilities.getCookie(cookieName);
+    var url = window.location.href;
+    var isPrivacyPage = url.includes('privacy-policy');
 
-    if (!isCookie || isCookie == null) {
-        var modal = '#privacy-policy-modal';
-        var _ = $(`${modal}`).modal('show');
-        var endpoint = '/privacy-policy/content';
-        var contentIdentifier = 'js-privacy-policy-modal-body';
-        var content = document.getElementById(contentIdentifier);
-        var accpetIdentifier = 'accept-cookie';
-        var accept = document.getElementById(accpetIdentifier);
-        var setCookie = function () {
-            var cookie = {
-                name: cookieName,
-                value: true,
-                age: 30,
+    if (!isPrivacyPage) {
+        if (!isCookie || isCookie == null) {
+            var modal = '#privacy-policy-modal';
+            var _ = $(`${modal}`).modal('show');
+            var endpoint = '/privacy-policy/content';
+            var contentIdentifier = 'js-privacy-policy-modal-body';
+            var content = document.getElementById(contentIdentifier);
+            var accpetIdentifier = 'accept-cookie';
+            var accept = document.getElementById(accpetIdentifier);
+            var setCookie = function () {
+                var cookie = {
+                    name: cookieName,
+                    value: true,
+                    age: 30,
+                };
+                var days = cookie.age;
+                var length = days * 24 * 60 * 60;
+                document.cookie = `${cookie.name}=${cookie.value}; max-age=${length}`;
             };
-            var days = cookie.age;
-            var length = days * 24 * 60 * 60;
-            document.cookie = `${cookie.name}=${cookie.value}; max-age=${length}`;
-        };
 
-        accept.addEventListener("click", setCookie);
+            accept.addEventListener("click", setCookie);
 
-        fetch(endpoint)
-            .then(function (response) {
-                return response.text();
-            })
-            .then(function (html) {
-                content.innerHTML = html;
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+            fetch(endpoint)
+                .then(function (response) {
+                    return response.text();
+                })
+                .then(function (html) {
+                    content.innerHTML = html;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
     }
 });
