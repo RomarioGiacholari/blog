2cbb10f3360ba835c8adcbeec787533d557f9614
RomarioGiacholari <giacholari@gmail.com>
2020-05-24 15:03:39 +0100

    remove comments and cache posts.index query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,11 +7,6 @@ use App\Post;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('admin');
