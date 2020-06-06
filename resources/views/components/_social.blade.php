08153ed1bc3ca4d29ec6d40ff544e6a62462d464
RomarioGiacholari <giacholari@gmail.com>
2020-06-06 14:04:54 +0100

    add social links at top of page and move welcome picture to about page

---
+++ resources/views/components/_social.blade.php
@@ -0,0 +1,8 @@
+<div class="social">
+    <a style="padding:10px" href="tel:+447587735380" title="call"><i class="fa fa-phone-square fa-2x"></i></a>
+    <a href="mailto:giacholari@gmail.com" title="email"><i class="fa fa-envelope fa-2x"></i></a>
+    <a href="https://www.instagram.com/am.giacholari/" title="instagram"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+    <a href="https://github.com/RomarioGiacholari" title="github"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+    <a href="https://uk.linkedin.com/in/romario-giacholari-71130b11b" title="linkedin"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+    <a href="{{ route('privacy-policy.index') }}">&copy; giacholari</a>
+</div>
