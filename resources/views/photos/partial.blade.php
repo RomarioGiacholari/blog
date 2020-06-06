a4d234e7fe88607f732e893181c41f206d71bbc4
RomarioGiacholari <giacholari@gmail.com>
2020-06-06 14:18:43 +0100

    set width to 100% for each photos partial

--- resources/views/photos/partial.blade.php
+++ resources/views/photos/partial.blade.php
@@ -1,7 +1,7 @@
 @if($viewModel != null && $viewModel->photos != null && count($viewModel->photos) > 0)
 @foreach($viewModel->photos as $name => $path)
 <div class="thumbnail white-panel">
-    <img src="{{ $path }}" title="{{ $name }}" alt="{{ $name }}">
+    <img src="{{ $path }}" title="{{ $name }}" alt="{{ $name }}" width="100%">
 </div>
 @endforeach
 @endif
