0313605a3453759b62b951668fb0c687719680b7
RomarioGiacholari <giacholari@gmail.com>
2020-06-11 14:47:37 +0100

    set width to 100% on projects and change about layout

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -11,17 +11,17 @@
     </div>
     <div class="row">
         <div class="col-md-4 text-center">
-            <img style="padding:10px;" class="img-thumbnail" id="thumbnail" src="https://assets.giacholari.com/images/gallery/me-driving-certificate.jpg" height="450" width="350" title="Romario Giacholari" alt="Romario Giacholari" />
+            <img class="img-responsive" id="thumbnail" src="https://assets.giacholari.com/images/gallery/me-driving-certificate.jpg" height="450" width="350" title="Romario Giacholari" alt="Romario Giacholari" />
         </div>
-        <div class="col-md-8">
-            <p class="font-size-16" style="padding:20px;">
+        <div class="col-md-8" style="margin-top:23px">
+            <blockquote class="font-size-16">
                 Thank you for visiting the site. My name is Romario and I am a web developer based in Birmingham, UK. 
                 My passion is web development and I really enjoy solving programming tasks.
                 Apart from that, I am also a big fan of football - I dare you challenge me on a 5-aside game :).
                 I come from Albania. However I grew up in Kardamyli, a small village south of Greece.
                 This summer I am about to graduate from Aston University - BSC Computing with business.
                 If you are interested in working together, do not hesitate to contact me.
-            </p>
+            </blockquote>
         </div>
     </div>
 </div>
