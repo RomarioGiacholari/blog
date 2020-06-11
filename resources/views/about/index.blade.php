5a2b4543e18f513ed90c959b0937c7eb8f287d50
RomarioGiacholari <giacholari@gmail.com>
2020-06-11 14:53:26 +0100

    add contact me link

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -20,7 +20,7 @@
                 Apart from that, I am also a big fan of football - I dare you challenge me on a 5-aside game :).
                 I come from Albania. However I grew up in Kardamyli, a small village south of Greece.
                 This summer I am about to graduate from Aston University - BSC Computing with business.
-                If you are interested in working together, do not hesitate to contact me.
+                If you are interested in working together, do not hesitate to <a href="{{ route('contact.create') }}">contact me</a>.
             </blockquote>
         </div>
     </div>
