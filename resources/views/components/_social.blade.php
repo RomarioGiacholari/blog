0c99c5b39bf88d13d66fb3a3e7a8784cd70bd987
RomarioGiacholari <giacholari@gmail.com>
2020-06-18 18:23:57 +0100

    remove social inline style

--- resources/views/components/_social.blade.php
+++ resources/views/components/_social.blade.php
@@ -1,5 +1,5 @@
 <div class="social">
-    <a style="padding:10px" href="tel:+447587735380" title="call"><i class="fa fa-phone-square fa-2x"></i></a>
+    <a href="tel:+447587735380" title="call"><i class="fa fa-phone-square fa-2x"></i></a>
     <a href="mailto:giacholari@gmail.com" title="email"><i class="fa fa-envelope fa-2x"></i></a>
     <a href="https://www.instagram.com/am.giacholari/" title="instagram"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
     <a href="https://github.com/RomarioGiacholari" title="github"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
