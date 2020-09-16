c29883b78f9ea420216da2947124994c321d76fc
RomarioGiacholari <romario.giacholari@tillo.io>
2020-09-16 20:39:26 +0100

    install doctrine dbal and modify posts table - decrease fields size plus amend related factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,16 +1,16 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(App\Post::class, function (Faker $faker) {
-    
     $title = $faker->title;
-    $body =  $faker->paragraph;
+    $body =  $faker->word;
 
     return [
-        'title' => $title,
-        'body' => $body,
-        'excerpt' => $body,
-        'slug' => $title,
+        'title'   => $title,
+        'body'    => $body,
+        'excerpt' => strip_tags(Str::words($body, 20, ' ...')),
+        'slug'    => str_slug($title, '-'),
     ];
 });
