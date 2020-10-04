958a7d5f49a68b567f118bf5ffe5592897ed50b3
vagrant <vagrant@blog>
2020-10-04 18:17:49 +0000

    use mysql for testing and update phpunit.xml

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use App\Post;
 use App\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 /**
@@ -12,7 +12,7 @@ use Tests\TestCase;
  */
 class PostTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_an_authenticated_user_can_create_a_post()
     {
