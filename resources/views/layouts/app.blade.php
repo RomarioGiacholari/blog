55fb9c411ab89a797a179a01ed9eb9b8e20a1770
Romario Giacholari <giacholari@gmail.com>
2020-10-08 18:36:06 +0100

    yield meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="Romario Giacholari">
-    <meta property="og:site_name" content="Romario Giacholari">
-    @yield('meta-tags')
+    @yield('meta-tags', View::make('components._meta-tags'))
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
