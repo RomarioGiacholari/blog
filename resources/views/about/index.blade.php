2b0bfcb0a4fed8eee4a937ba327b35b8825ee4bf
Romario Giacholari <giacholari@gmail.com>
2020-10-15 17:03:43 +0100

    tidy up welcome view and increase font size in both welcome and about page

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -10,10 +10,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-4 text-center">
-            <img class="img-responsive" id="thumbnail" src="https://assets.giacholari.com/images/gallery/me-driving-certificate.jpg" height="450" width="350" title="Romario Giacholari" alt="Romario Giacholari" />
-        </div>
-        <div class="col-md-8" style="margin-top:23px">
+        <div class="col-md-12">
             <p class="font-size-16">
                 Thank you for visiting the site. My name is Romario and I am a web developer based in Brighton, UK.
                 My passion is web development and I really enjoy solving programming tasks.
@@ -22,6 +19,9 @@
                 I am officially now a graduate from Aston University - BSc Computing for Business.
                 If you are interested in working together or just because you want to get in touch, do not hesitate to <a href="{{ route('contact.create') }}"><u>contact me</u></a>.
             </p>
+        </div>
+        <div class="col-md-12">
+            <img class="img-responsive" id="thumbnail" src="https://assets.giacholari.com/images/gallery/me-driving-certificate.jpg" height="450" width="350" title="Romario Giacholari" alt="Romario Giacholari" />
         </div>
     </div>
 </div>
