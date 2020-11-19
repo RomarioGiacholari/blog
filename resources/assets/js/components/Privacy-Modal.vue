549d90bf036bf3a74d4907a58bc12c7bea75b689
Romario Giacholari <giacholari@gmail.com>
2020-11-19 17:57:46 +0000

    make privacy modal static

--- resources/assets/js/components/Privacy-Modal.vue
+++ resources/assets/js/components/Privacy-Modal.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="modal fade" id="privacy-policy-modal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal fade" id="privacy-policy-modal" tabindex="-1" data-backdrop="static" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
           <h5 class="modal-title">Privacy policy</h5>
         </div>
         <div id="js-privacy-policy-modal-body" class="modal-body" v-html="content"></div>
