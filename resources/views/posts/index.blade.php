ec3c272ebbf477673e73b8e090aebb28bc4efe5c
Romario Giacholari <giacholari@gmail.com>
2021-02-20 14:21:26 +0000

    validate in the frontend as well

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -37,11 +37,18 @@
 @section('scripts')
 <script defer>
     document.addEventListener("DOMContentLoaded", function () {
+        var allowedKeysForOrderBy = ['created_at', 'views'];
+        var defaultOrderBy = allowedKeysForOrderBy[0];
         var orderByInput = document.getElementById("orderBy");
+
         orderByInput.addEventListener("change", function () {
             var currentValue = this.value;
 
             if (currentValue !== undefined && currentValue !== '') {
+                if (!allowedKeysForOrderBy.includes(currentValue)) {
+                    currentValue = defaultOrderBy;
+                }
+
                 var uri = "/posts?orderBy=" + currentValue;
                 var encodedUri = encodeURI(uri);
                 window.location.href = encodedUri;
