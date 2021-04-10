a27b253065115cccb9d5cd8236bc8f9c006e6263
vagrant <vagrant@homestead>
2021-04-10 18:08:04 +0000

    add setters on the Pagination View Model to handle negative integers

--- app/ViewModels/Pagination/PaginationViewModel.php
+++ app/ViewModels/Pagination/PaginationViewModel.php
@@ -9,7 +9,25 @@ class PaginationViewModel
 
     public function __construct(int $page, int $totalPages)
     {
-        $this->page = $page;
-        $this->totalPages = $totalPages;
+        $this->page = static::setPage($page);
+        $this->totalPages = static::setTotalPages($totalPages);
+    }
+
+    private static function setPage(int $page): int
+    {
+        if ($page < 1) {
+            $page = 1;
+        }
+
+        return $page;
+    }
+
+    private static function setTotalPages(int $totalPages): int
+    {
+        if ($totalPages < 1) {
+            $totalPages = 1;
+        }
+
+        return $totalPages;
     }
 }
