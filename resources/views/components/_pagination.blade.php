4349dd79ed010c38d6da6bc4614e57890076dfb4
vagrant <vagrant@homestead>
2021-04-10 12:22:12 +0000

    add page / total pages

--- resources/views/components/_pagination.blade.php
+++ resources/views/components/_pagination.blade.php
@@ -2,5 +2,6 @@
     <div id="pagination" data-pagination="{{json_encode($viewModel->pagination)}}">
         <button id="previous">previous</button>
         <button id="next">next</button>
+        <span>{{ $viewModel->pagination->page }} / {{ $viewModel->pagination->totalPages }}</span>
     </div>
 @endif
