2e03c5aa708ee2e1bc8f0f2e4bf0f2a19ae4d147
vagrant <vagrant@homestead>
2021-04-11 10:21:30 +0000

    extract controller methods to adapter and check if there any items to determine the total pages: default to one

--- app/Adapters/Post/PostRequestAdapter.php
+++ app/Adapters/Post/PostRequestAdapter.php
@@ -22,4 +22,41 @@ class PostRequestAdapter
 
         return $postEntity;
     }
+
+    public static function getOrderByKey(Request $request): string
+    {
+        $allowedKeys = ['created_at', 'views'];
+        $default = $allowedKeys[0];
+        $orderBy = $request->query('order_by') ?? $default;
+
+        if (!in_array($orderBy, $allowedKeys)) {
+            $orderBy = $default;
+        }
+
+        return $orderBy;
+    }
+
+    public static function getOrderByDirection(Request $request): string
+    {
+        $allowedKeys = ['desc', 'asc'];
+        $default = $allowedKeys[0];
+        $direction = $request->query('direction') ?? $default;
+
+        if (!in_array($direction, $allowedKeys)) {
+            $direction = $default;
+        }
+
+        return $direction;
+    }
+
+    public static function getPage(Request $request): int
+    {
+        $page =  $request->query('page') ?? 1;
+
+        if ($page < 1) {
+            $page = 1;
+        }
+
+        return $page;
+    }
 }
