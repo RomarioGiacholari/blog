4d9291c4fc2e697bd243993485a6e7cd6f58bc4e
Romario Giacholari <giacholari@gmail.com>
2021-05-28 13:30:17 +0100

    wrap privacy vue component within a blade block

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,8 +31,8 @@
         @include('components._divider')
         @include('navbar.navbar')
         @include('components._noscript')
+        @include('components._privacy')
         @yield('content')
-        <privacy-modal></privacy-modal>
     </div>
     <script defer src="https://assets.giacholari.com/js/blog/app.js"></script>
     <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "24b18c23c53140d5a67de4a052d255cb"}'></script>
