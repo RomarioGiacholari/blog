78069af93f76b226a239b9b8d436092b70727a3b
Romario Giacholari <giacholari@gmail.com>
2021-06-12 15:59:15 +0100

    update about information

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -12,10 +12,9 @@
     <div class="row">
         <div class="col-md-12">
             <p class="font-size-16">
-                Thank you for visiting the site. My name is Romario and I am a web developer based in Brighton, UK.
+                Thank you for visiting the site. My name is Romario and I am a web developer based in England.
                 My passion is web development and I really enjoy solving programming tasks.
-                Apart from that, I am also a big fan of football - I dare you challenge me on a 5-aside game :)
-                I come from Albania. However, I grew up in Kardamyli, a small village which is located south of Greece.
+                Apart from that, I am also a big fan of football - I dare you challenge me on a 5-aside game.
                 I am officially now a graduate from Aston University - BSc Computing for Business.
                 If you are interested in working together or just because you want to get in touch, do not hesitate to <a href="{{ route('contact.create') }}"><u>contact me</u></a>.
             </p>
