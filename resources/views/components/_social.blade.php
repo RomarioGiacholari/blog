2e89c6423e776d532b39df8b22b3ba58f42330c4
Romario Giacholari <giacholari@gmail.com>
2021-07-09 18:16:02 +0100

    update text

--- resources/views/components/_social.blade.php
+++ resources/views/components/_social.blade.php
@@ -1,5 +1,5 @@
 <div class="social text-center" style="background-color:#222;padding:10px;">
-    <a href="https://www.instagram.com/am.giacholari/" title="instagram"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+    <a href="https://www.instagram.com/giacholari/" title="instagram"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
     <a href="https://github.com/RomarioGiacholari" title="github"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
     <a href="https://uk.linkedin.com/in/romario-giacholari-71130b11b" title="linkedin"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
 </div>
